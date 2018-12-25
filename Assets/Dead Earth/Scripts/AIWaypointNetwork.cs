81c185be94ba2aa8b59ccc3854356444b388e091
tensor-flower <wanqiguo2@gmail.com>
2018-12-25 16:51:42 +0800

    first commit

---
+++ Assets/Dead Earth/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIWaypointNetwork : MonoBehaviour {
+
+	public List<Transform> Waypoints = new List<Transform>();
+}
