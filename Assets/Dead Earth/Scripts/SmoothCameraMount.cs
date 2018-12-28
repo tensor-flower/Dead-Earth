c51fb5c704943633071ed67c270a54e8fbdd3e98
tensor-flower <wanqiguo2@gmail.com>
2018-12-28 22:25:59 +0800

    characters and animation untracked from now on, set up mecanim

---
+++ Assets/Dead Earth/Scripts/SmoothCameraMount.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraMount : MonoBehaviour {
+
+	[SerializeField] Transform cameraPos;
+	private float _speed = 5f;
+
+	void Start () {
+		
+	}
+	
+	void LateUpdate () {
+		transform.position = Vector3.Lerp(transform.position, cameraPos.position, Time.deltaTime * _speed);
+		transform.rotation = Quaternion.Slerp(transform.rotation, cameraPos.rotation, Time.deltaTime * _speed);
+	}
+}
---
+++ Assets/Dead Earth/Scripts/SpeedController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour {
+
+	Animator animator;
+	private int _horizontalHash;
+	private int _verticalHash;
+	private int _attackHash;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+		_horizontalHash = Animator.StringToHash("Horizontal");
+		_verticalHash = Animator.StringToHash("Vertical");
+		_attackHash = Animator.StringToHash("Attack");
+	}
+	
+	void Update () {
+		float horizontalThrow = Input.GetAxis("Horizontal") * 2.3f;
+		float verticalThrow = Input.GetAxis("Vertical") * 5.66f;
+		//Debug.Log(horizontalThrow + ", " + verticalThrow);
+		animator.SetFloat(_horizontalHash, horizontalThrow, 0.1f, Time.deltaTime);
+		animator.SetFloat(_verticalHash, verticalThrow, 1f, Time.deltaTime);
+		if(Input.GetKeyDown("space"))
+			animator.SetTrigger(_attackHash);
+	}
+}
